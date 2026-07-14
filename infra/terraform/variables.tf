54fa9f3d77ae1c37af01063f25b39a088e7f53fc
vrmalazarte <vannnmalazarte@gmail.com>
2026-07-15 01:32:14 +0800

    add basic terraform configuration
    - add initial terraform configuration
    - add provider configuration
    - add variables and outputs
    - add terraform lock file
    - update .gitignore for terraform state files

---
+++ infra/terraform/variables.tf
@@ -0,0 +1,11 @@
+variable "project_id" {
+  description = "Google Cloud project ID"
+  type        = string
+  default     = "project-9236686f-9fd7-4cd9-a31"
+}
+
+variable "region" {
+  description = "Google Cloud region"
+  type        = string
+  default     = "asia-southeast1"
+}
